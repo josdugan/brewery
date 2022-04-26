9d54197216e4cc2f282de2184e213135d11de856
josdugan <josdugan@gmail.com>
2022-04-26 19:47:24 -0400

    Initial commit

---
+++ src/test/java/com/josdugan/brewery/brewery/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josdugan.brewery.brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
