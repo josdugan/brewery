efaf513abb9594372c7881fe23910365c3d92338
josdugan <josdugan@gmail.com>
2022-04-27 18:08:54 -0400

    Add CUD to CustomerController

--- src/main/java/com/josdugan/brewery/web/service/CustomerServiceImpl.java
+++ src/main/java/com/josdugan/brewery/web/service/CustomerServiceImpl.java
@@ -1,10 +1,12 @@
 package com.josdugan.brewery.web.service;
 
 import com.josdugan.brewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService {
     @Override
@@ -13,5 +15,22 @@ public class CustomerServiceImpl implements CustomerService {
                 .id(UUID.randomUUID())
                 .name("Bipin")
                 .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+
+    }
+
+    @Override
+    public void deleteCustomer(UUID customerId) {
+
     }
 }
